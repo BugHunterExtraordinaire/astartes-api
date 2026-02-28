9226d39cf280e49931ec97dc81a4cfa458830490
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-02-28 23:50:27 +0200

    Connect to database successfully

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const connectDB = require('./database/connect');
+
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is running on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
---
+++ database/connect.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const connectDB = async (uri) => {
+  return await mongoose.connect(uri, {
+    dbName: "CODEX ASTARTES"
+  })
+};
+
+module.exports = connectDB;
