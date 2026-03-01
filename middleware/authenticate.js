3e66e63d820dae068cd529c752b151c197962d96
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-03-01 10:53:07 +0200

    Finish authentication middleware

---
+++ middleware/authenticate.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const { NotAuthenticatedError } = require('../errors')
+
+const authenticateUser = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) return next(new NotAuthenticatedError('Not authenticated to access this route'));
+  const token = authHeader.split(' ')[1];
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = payload;
+    next();
+  } catch (error) {
+    return next(new NotAuthenticatedError('Token is invalid')); 
+  }
+}
+
+module.exports = authenticateUser;
