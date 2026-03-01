2f92e7520a5187de66d7f1eada18f340c1d4e899
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-03-01 05:57:19 +0200

    Added enum chapter values and automatic isTraitor validation

--- models/marine.js
+++ models/marine.js
@@ -9,17 +9,39 @@ const marineSchema = new mongoose.Schema({
   chapter: {
     type: String,
     required: [true, "Please provide Astartes chapter"],
-    minlength: 5
+    enum: ["the Emperor's Children", "World Eaters", "Death Guard", "Thousand Sons", 
+           "Black Legion", "Word Bearers", "Iron Warriors", "Night Lords", 
+           "Alpha Legion", "Black Templars", "Crimson Fists", "Dark Angels", 
+           "Blood Angels", "White Scars", "Imperial Fists", "Iron Hands", 
+           "Ultramarines", "Salamanders", "Raven Gaurd"]
   },
   rank: {
     type: String,
-    enum: ["Aspirant", "Neophyte", "Battle Brother", "Sargeant", "Lieutenant", "Captain", "Chapter Master"],
+    enum: ["Aspirant", "Neophyte", "Vanguard Space Marine", "Battle Brother", 
+           "Sergeant", "Lieutenant", "Captain", "Chapter Master", 
+           "Librarian", "Chaplain", "Apothecary", "Techmarine", 
+           "Master of the Signal", "Chapter Ancient", "Judiciar"],
     default: "Battle Brother",
   },
   isTraitor: {
     type: Boolean,
     default: false,
   },
+  isActive: {
+    type: Boolean,
+    default: true,
+  }
 });
+
+marineSchema.pre('save', function() {
+  const traitorMarines = ["The Emperor's Children", "World Eaters", "Death Guard", 
+                          "Thousand Sons", "Black Legion", "Word Bearers", 
+                          "Iron Warriors", "Night Lords", "Alpha Legion"];
+  traitorMarines.forEach(traitor => {
+    if (traitor === this.chapter) {
+      this.isTraitor = true;
+    }
+  });
+})
 
 module.exports = mongoose.model('Marine', marineSchema);
