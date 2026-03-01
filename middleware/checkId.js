cd686ad89ec9b0bed1b4bbf90a1d3444571e5d43
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-03-01 11:29:34 +0200

    Fix naming conflict bug where marineId was referenced as astartesId

--- middleware/checkId.js
+++ middleware/checkId.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const checkId = (req, res, next) => {
   const { id: marineId } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(astartesId)) return next(new BadRequestError(`please provide a valid id`));
+  if (!mongoose.Types.ObjectId.isValid(marineId)) return next(new BadRequestError(`please provide a valid id`));
   req.marineId = marineId;
   next();
 }
